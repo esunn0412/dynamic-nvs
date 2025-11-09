34c8ae0a0997332d55fe9d708e21ec0055b6f183
esunn0412 <taeeunk1208@gmail.com>
2025-11-09 08:06:37 +0000

    eval one done

--- scripts/run_demo.sh
+++ scripts/run_demo.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=/scratch/tkim462/vision/scripts/logs/demo_%j.out
 #SBATCH --error=/scratch/tkim462/vision/scripts/logs/demo_%j.err
 #SBATCH --time=02:00:00
-#SBATCH --partition=l4-4-gm96-c48-m192
+#SBATCH --partition=a100-8-gm320-c96-m1152
 #SBATCH --nodes=1
 #SBATCH --gpus=1
 #SBATCH --ntasks=1
@@ -24,8 +24,20 @@ echo "SLURM_JOB_ID: ${SLURM_JOB_ID:-N/A}"
 # Run demo
 python /scratch/tkim462/vision/demo.py \
     --model_path "/scratch/tkim462/vision/models/CogVideoX-5b-I2V" \
-    --cognvs_ckpt_path "/scratch/tkim462/vision/models/checkpoints/cognvs_ckpt_finetuned_davis_bear/my_checkpoint-200_transformer" \
-    --data_path "/scratch/tkim462/vision/demo_data/davis_bear" \
-    --mp4_name "example_eval_render.mp4"
+    --cognvs_ckpt_path "/scratch/tkim462/vision/models/checkpoints/cognvs_ckpt_finetuned_rodtang/my_checkpoint-200_transformer" \
+    --data_path "/scratch/tkim462/vision/demo_data/rodtang" \
+    --mp4_name "eval_render2.mp4"
+
+python /scratch/tkim462/vision/demo.py \
+    --model_path "/scratch/tkim462/vision/models/CogVideoX-5b-I2V" \
+    --cognvs_ckpt_path "/scratch/tkim462/vision/models/checkpoints/cognvs_ckpt_finetuned_rodtang/my_checkpoint-200_transformer" \
+    --data_path "/scratch/tkim462/vision/demo_data/rodtang" \
+    --mp4_name "eval_render3.mp4"
+
+python /scratch/tkim462/vision/demo.py \
+    --model_path "/scratch/tkim462/vision/models/CogVideoX-5b-I2V" \
+    --cognvs_ckpt_path "/scratch/tkim462/vision/models/checkpoints/cognvs_ckpt_finetuned_rodtang/my_checkpoint-200_transformer" \
+    --data_path "/scratch/tkim462/vision/demo_data/rodtang" \
+    --mp4_name "eval_render4.mp4"
 
 echo "Job finished: $(date)"
